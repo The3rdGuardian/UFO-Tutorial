93c3017183b6400f3f9a57172d77525de3b13478
Edward <edwardddpowers3@knights.ucf.edu>
2019-09-21 23:47:40 -0400

    UFO Challenge update

--- New Unity Project/Assets/Scripts/PlayerControl.cs
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -6,21 +6,34 @@ using UnityEngine.UI;
 public class PlayerControl : MonoBehaviour
 {
 
-    public float speed;               
-    public Text countText;           
+    public float speed;
+
+    public Text livesText;
+    public Text countText;
+
+
     public Text winText;
-    public Text WinText2;
+    public Text winText2;
+    public Text loseText;
 
     private Rigidbody2D rb2d;      
-    private int count;                
+    private int count;
+    private int lives;
 
  
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
         count = 0;
+        lives = 3;
+
         winText.text = "";
-        WinText2.text = "";
+        winText2.text = "";
+        loseText.text = "";
+
+        SetLivesText();
+    
         SetCountText();
     }
 
@@ -32,34 +45,64 @@ public class PlayerControl : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
 
-        if (Input.GetKey("escape"))
-        {
-            Application.Quit();
-        }
-           
+        
+        SetLivesText();
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("PickUp"))
+        
+        if (other.gameObject.CompareTag("Pentagon"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
+            count++;
             SetCountText();
         }
 
-           
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives--;
+            SetLivesText();
+
+        }
+        if (count == 6 && lives >= 1)
+        {
+            transform.position = new Vector2(66f, 0f);
+        }
+
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+       
+        if (lives < 1)
+        {
+            loseText.text = "You have lost the game! Better luck next time!";
+            rb2d.bodyType = RigidbodyType2D.Static;
+        }
     }
 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        
+
+        if (lives >= 1 && count == 14)
         {
-            winText.text = "You Win!";
-            WinText2.text = "Game created by Edward Powers";
+            winText.text = "Congratulations! You Win!";
+            winText2.text = "Game created by Edward Powers";
+            rb2d.bodyType = RigidbodyType2D.Static;
         }
+        else
+        {
+            SetLivesText();
+        }
+
+       
     }
-          
+
 }
 
